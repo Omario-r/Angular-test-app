95ab78c18b1637376ad78db82960f2ca9f1502f8
Avilov. R <aviloff@yandex.ru>
2020-01-12 00:07:04 +1000

    fix(): move list data in test component

--- src/app/test/modal/modal.component.ts
+++ src/app/test/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Inject} from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
@@ -11,19 +11,7 @@ export class ModalComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<ModalComponent>,
-    // @Inject(MAT_DIALOG_DATA)
-    ) {}
-
-    data = [
-      { name:'Episode I - The Phantom Menace', show: true },
-      { name:'Episode II - Attack of the Clones', show: true },
-      { name:'Episode III - Revenge of the Sith', show: true },
-      { name:'Episode IV - A New Hope', show: true },
-      { name:'Episode V - The Empire Strikes Back', show: true },
-      { name:'Episode VI - Return of the Jedi', show: true },
-      { name:'Episode VII - The Force Awakens', show: true },
-      { name:'Episode VIII - The Last Jedi', show: true }
-    ];
+    @Inject(MAT_DIALOG_DATA) public data) {}
 
     eyeShow = true;
   
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -12,16 +12,27 @@ export class TestComponent implements OnInit {
 
   constructor(public dialog: MatDialog) { }
 
+  data = [
+    { name:'Episode I - The Phantom Menace', show: true },
+    { name:'Episode II - Attack of the Clones', show: true },
+    { name:'Episode III - Revenge of the Sith', show: true },
+    { name:'Episode IV - A New Hope', show: true },
+    { name:'Episode V - The Empire Strikes Back', show: true },
+    { name:'Episode VI - Return of the Jedi', show: true },
+    { name:'Episode VII - The Force Awakens', show: true },
+    { name:'Episode VIII - The Last Jedi', show: true }
+  ];
+
   openDialog(): void {
     const dialogRef = this.dialog.open(ModalComponent, {
       width: '550px',
-      panelClass: 'modal-class'
-      // data: {name: this.name, animal: this.animal}
+      panelClass: 'modal-class',
+      data: this.data
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      // this.animal = result;
+      console.log('The dialog was closed', this.data);
+      
     });
   }
 
